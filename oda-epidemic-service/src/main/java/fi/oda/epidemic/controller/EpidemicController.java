beeba4faf654fdb335e91522f433b1712fde1b24
Okko Kauhanen <okko.kauhanen@mediconsult.fi>
2017-05-05 20:42:10 +0300

    Missing import

--- oda-epidemic-service/src/main/java/fi/oda/epidemic/controller/EpidemicController.java
+++ oda-epidemic-service/src/main/java/fi/oda/epidemic/controller/EpidemicController.java
@@ -1,5 +1,6 @@
 package fi.oda.epidemic.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
