2b06ce9574411be2025233ecd38d912aa8f59c0a
Okko Kauhanen <okko.kauhanen@mediconsult.fi>
2017-05-09 11:41:39 +0300

    Added init script for docker

---
+++ init-script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+nohup java -jar /opt/services/oda-dss-service/oda-dss-service.jar &
+nohup java -jar /opt/services/oda-epidemic-service/oda-epidemic-service.jar &
+cd /tmp/mc-solmu/web-client
+npm install
+nohup npm run start &
