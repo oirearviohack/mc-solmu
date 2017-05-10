348acc3757f962f4186fbea1ee925c6b96589476
Okko Kauhanen <okko.kauhanen@mc.local>
2017-05-10 11:29:58 +0100

    trying with different settings

--- init-script.sh
+++ init-script.sh
@@ -2,5 +2,6 @@
 nohup java -jar /opt/services/oda-dss-service/oda-dss-service.jar &
 nohup java -jar /opt/services/oda-epidemic-service/oda-epidemic-service.jar &
 cd /tmp/mc-solmu/web-client
-npm install
-nohup npm run start &
+#npm install
+
+#nohup npm run start &
