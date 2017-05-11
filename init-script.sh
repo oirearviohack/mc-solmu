d95ef5e77dbee5c1acd0a1df4ec471fa7fe0e636
Okko Kauhanen <okko.kauhanen@mediconsult.fi>
2017-05-11 14:04:55 +0300

    trying settings

--- init-script.sh
+++ init-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 nohup java -jar /opt/hack/oda-dss-service/oda-dss-service.jar &
 nohup java -jar /opt/hack/oda-epidemic-service/oda-epidemic-service.jar &
-cd /opt/hack/front
+cd /opt/hack/mc-solmu/web-client
 npm install
 npm run build
 npm run start:server
