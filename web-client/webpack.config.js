32f1f87de226f4425144ae09e1482cac42debf19
Okko Kauhanen <okko.kauhanen@mediconsult.fi>
2017-05-11 12:10:18 +0300

    kokeillaan asetuksia

--- web-client/webpack.config.js
+++ web-client/webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
 
   entry: [
     'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:8081',
+    'webpack-dev-server/client?http://0.0.0.0:8081',
     'webpack/hot/only-dev-server',
     'whatwg-fetch',
     './index.js'
