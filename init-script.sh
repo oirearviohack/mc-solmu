fba3dc106cf6642d4b62d6c57adf3022306a9da7
Okko Kauhanen <okko.kauhanen@mediconsult.fi>
2017-05-11 12:15:53 +0300

    testataan asetuksia

--- init-script.sh
+++ init-script.sh
@@ -3,5 +3,6 @@ nohup java -jar /opt/services/oda-dss-service/oda-dss-service.jar &
 nohup java -jar /opt/services/oda-epidemic-service/oda-epidemic-service.jar &
 cd /tmp/mc-solmu/web-client
 npm install
+npm run build
 npm run start:server
 
